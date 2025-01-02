3ac9eee12a3486a0ee9c931008fd45eab339b8ca
AlphaR2 <danielfrancis32610@gmail.com>
2025-01-02 16:46:01 +0100

    Feat: Embedded Walet and cost deduction

--- utils/config/baseUrl.ts
+++ utils/config/baseUrl.ts
@@ -7,3 +7,5 @@ export const baseURLDev = "https://poynt-ad-server.onrender.com/api/v1";
 // Choose the appropriate baseURL based on the environment
 export const baseURL =
   process.env.NODE_ENV === "production" ? baseURLProd : baseURLDev;
+
+
