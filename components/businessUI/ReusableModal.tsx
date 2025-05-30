92cd661eae188cf8abf6ab6734f03dd8a0fe5796
AlphaR2 <danielfrancis32610@gmail.com>
2025-05-30 18:01:21 +0100

    Continious Case Fix

--- components/businessUI/ReusableModal.tsx
+++ components/businessUI/ReusableModal.tsx
@@ -1,10 +1,10 @@
-import React, { ReactNode, FC } from 'react';
+import React, { ReactNode, FC } from "react";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
-} from '@/components/ui/Dialog';
+} from "@/components/ui/Dialog";
 
 interface ModalProps {
   isOpen: boolean;
@@ -13,18 +13,13 @@ interface ModalProps {
   children: ReactNode;
 }
 
-const ReModal: FC<ModalProps> = ({ 
-  isOpen,
-  onClose, 
-  title, 
-  children 
-}) => {
+const ReModal: FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
   return (
     <>
       {/* Backdrop */}
       {isOpen && (
-        <div 
-          className="fixed inset-0  bg-opacity-50 transition-opacity" 
+        <div
+          className="fixed inset-0  bg-opacity-50 transition-opacity"
           aria-hidden="true"
         />
       )}
@@ -33,20 +28,14 @@ const ReModal: FC<ModalProps> = ({
       <Dialog open={isOpen} onOpenChange={onClose}>
         <DialogContent className="fixed  w-[60%] max-w-4xl  -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-100 p-6 shadow-xl">
           <DialogHeader className="relative  border-b pb-6  border-gray-200">
-            <DialogTitle className="text-xl font-semibold">
-              {title}
-            </DialogTitle>
-           
+            <DialogTitle className="text-xl font-semibold">{title}</DialogTitle>
           </DialogHeader>
 
-          <div className="overflow-y-auto py-4 h-full">
-            {children}
-          </div>
+          <div className="overflow-y-auto py-4 h-full">{children}</div>
         </DialogContent>
       </Dialog>
     </>
   );
 };
-
 
 export default ReModal;
